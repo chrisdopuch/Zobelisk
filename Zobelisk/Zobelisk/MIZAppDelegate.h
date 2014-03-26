8d9487e09c46359d6de5f84763ff4b5687faf6dc
Clifford Green <Green@Cliffords-MacBook-Pro.local>
2014-03-26 13:16:50 -0500

    created initial xCode files

---
+++ Zobelisk/Zobelisk/MIZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MIZAppDelegate.h
+//  Zobelisk
+//
+//  Created by Clifford Green on 3/26/14.
+//  Copyright (c) 2014 Mizzou IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MIZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Zobelisk/Zobelisk/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Zobelisk
+//
+//  Created by Clifford Green on 3/26/14.
+//  Copyright (c) 2014 Mizzou IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MIZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MIZAppDelegate class]));
+    }
+}
