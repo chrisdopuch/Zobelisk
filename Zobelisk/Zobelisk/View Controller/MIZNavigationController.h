3c55eeebe78675fa56e874b8a61317f58d82ca01
Clifford Green <Green@cliffords-mbp.pk5001z>
2014-03-26 19:21:42 -0500

    lowered warnings only 4 remain

---
+++ Zobelisk/Zobelisk/View Controller/MIZNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  MIZNavigationController.h
+//  Zobelisk
+//
+//  Created by Clifford Green on 3/26/14.
+//  Copyright (c) 2014 Mizzou IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MIZNavigationController : UINavigationController
+
+@end
