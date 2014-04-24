2691728eabdee7f7b581b96acb488235c18cff10
Seth <sjw3kf@mail.missouri.edu>
2014-04-24 18:22:18 -0500

    registration

---
+++ Zobelisk/Zobelisk/Model/MIZAuthentication.h
@@ -0,0 +1,17 @@
+//
+//  MIZAuthentication.h
+//  Zobelisk
+//
+//  Created by Seth Wiesman on 4/23/14.
+//  Copyright (c) 2014 Mizzou IT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MIZAuthentication : NSObject
+
++ (void) registerEmail:(NSString*)email withPassword:(NSString*)password andConfirmationPassword:(NSString*)password_confirmation;
+
++ (void) loginWithEmail:(NSString*)email withPassword:(NSString*)password;
+
+@end
