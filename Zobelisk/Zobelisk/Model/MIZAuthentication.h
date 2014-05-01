14652a1213d9d53e85a11fa25d51ab3da6bf098a
Seth <sjw3kf@mail.missouri.edu>
2014-05-01 13:12:20 -0500

    full registration

--- Zobelisk/Zobelisk/Model/MIZAuthentication.h
+++ Zobelisk/Zobelisk/Model/MIZAuthentication.h
@@ -10,6 +10,8 @@
 
 @interface MIZAuthentication : NSObject
 
++ (void) registerEmail:(NSString*)email withPassword:(NSString*)password confirmationPassword:(NSString*)password_confirmation firstName:(NSString*)fName lastName:(NSString*)lname dateOfBirth:(NSDate*)dob address:(NSString*)loc twitter:(NSString*)twiterHandle andPhonenumber:(NSString*)number;
+
 + (void) registerEmail:(NSString*)email withPassword:(NSString*)password andConfirmationPassword:(NSString*)password_confirmation;
 
 //+ (void) loginWithEmail:(NSString*)email withPassword:(NSString*)password;
