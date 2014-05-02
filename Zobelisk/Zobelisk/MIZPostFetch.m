97025d6dabeef9d98b77e621e0565997f5d2bdd0
Seth <sjw3kf@mail.missouri.edu>
2014-05-01 20:33:26 -0500

    get posts by beacon

--- Zobelisk/Zobelisk/MIZPostFetch.m
+++ Zobelisk/Zobelisk/MIZPostFetch.m
@@ -7,6 +7,7 @@
 //
 
 #import "MIZPostFetch.h"
+#import "MIZGregorianDateComponents.h"
 
 @implementation MIZPostFetch
 
@@ -78,9 +79,10 @@
     
 }
 
-+ (void)fetchPost
++ (void)fetchPostforBeacon:(int)id
 {
-    NSString *urlString = @"http://zobelisk-backend.herokuapp.com/posts.json";
+    NSString *urlString = [NSString stringWithFormat:@"%@%d%@", @"http://zobelisk-backend.herokuapp.com/posts?beacon=", id, @".json"];
+    //NSString *urlString = @"http://zobelisk-backend.herokuapp.com/posts.json";
     NSURL *url = [NSURL URLWithString:urlString];
     NSURLSession *session = [NSURLSession sharedSession];
     [[session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
